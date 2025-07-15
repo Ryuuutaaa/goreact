dbdea24093011844579ac8c3381a2281bebe00f7
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-07-16 02:16:17 +0800

    feat  : get started api without db

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	fmt.Print("Hello world")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "hellow world"})
+
+	})
+
+	log.Fatal(app.Listen(":4000"))
 
 }
